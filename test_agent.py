4093d35d5b491b2475d90a220c36f63d4f8ea00c
barbaralococo2106 <barbaralococo2106>
2026-06-03 22:41:56 -0300

    fix: remove venv from tracking

--- test_agent.py
+++ test_agent.py
@@ -8,10 +8,7 @@ cv_file_path = "/Users/barbaralococo/Desktop/Trabajos/matchCV/cvmatch-api/CV _BÃ
 
 resultado = run_agent(
     cv_file_path=cv_file_path,
-    jd_text="""
-    We are looking for a Senior Data Scientist with experience in 
-    Python, SQL, machine learning, and A/B testing...
-    """
+    jd_text="""https://www.linkedin.com/jobs/search/?currentJobId=4384288184&f_C=74044131&geoId=92000000&origin=COMPANY_PAGE_JOBS_CLUSTER_EXPANSION&originToLandingJobPostings=4416306836%2C4384288184%2C4395924520%2C4390243175%2C4418753966%2C4401139934%2C4413042438%2C4403692783%2C4414175305&trk=d_flagship3_company_posts"""
 )
 
 print("SCORE:", resultado.get("score_compatibility", {}).get("overall_score"))
